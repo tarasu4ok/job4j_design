5a0e6bd7ddade1b133d4eb779b4d29400cd8ed13
tarasu4ok <taras.kramarenko@gmail.com>
2021-04-21 20:47:38 +0700

    0.1. FileOutputStream. [#494727]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,26 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                stringBuilder.append(i)
+                        .append(" * ")
+                        .append(j)
+                        .append(" = ")
+                        .append(i * j)
+                        .append("\n");
+            }
+            stringBuilder.append("\n");
+        }
+
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            out.write(stringBuilder.toString().getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
